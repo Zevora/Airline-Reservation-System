936ceeff031dc5d30c2caeb2399f5563c7390607
Zevora <34713514+Zevora@users.noreply.github.com>
2018-11-27 12:02:50 -0500

    Files for the Passenger Seat map
    adds the files "PassengerSeatMap.fxml", "PassengerSeatMapController.java" . That make up the Passenger Seat Map.

---
+++ PassengerSeatMapController.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package airplane;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+
+import javafx.beans.value.*;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author Lyan
+ */
+public class PassengerSeatMapController implements Initializable {
+
+        @FXML
+    private AnchorPane seatMapAnchorPane;
+
+    @FXML
+    private TextField flightNumberField;
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private Button generateButton;
+
+    @FXML
+    void goBack(ActionEvent event) throws IOException
+    {
+        AnchorPane pane = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        seatMapAnchorPane.getChildren().setAll(pane);
+    }
+
+    @FXML
+    void generate(ActionEvent event) throws IOException
+    {
+        //Display Passenger seat map
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
