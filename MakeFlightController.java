b7a890d11b5683db12469023bd29313d5c7f5879
JamesHolland181 <42613313+JamesHolland181@users.noreply.github.com>
2018-11-27 18:47:05 -0500

    MakeFlight 2.0
    Creates flight map (without unique name), sends flight info to file.
    
    **Possible issues**
    Flight info file may overwrite each other rather than appending to it.
    May prove better to make the 'makeMap' function able to be called from the other FXML controllers (unless it will already do this?).

--- MakeFlightController.java
+++ MakeFlightController.java
@@ -16,7 +16,6 @@ import java.util.ArrayList;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.Button;
@@ -24,8 +23,6 @@ import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 
 import javafx.beans.value.*;
-import javafx.scene.layout.AnchorPane;
-
 import static jdk.nashorn.internal.objects.ArrayBufferView.length;
 
 /**
@@ -34,9 +31,6 @@ import static jdk.nashorn.internal.objects.ArrayBufferView.length;
  * @author Lyan
  */
 public class MakeFlightController implements Initializable {
-
-    @FXML
-    private AnchorPane flightMakerPane;
 
     @FXML
     private TextField flightNum;
@@ -59,18 +53,11 @@ public class MakeFlightController implements Initializable {
     @FXML
     private TextField AvailableSeats;
 
-    @FXML Button backButton;
-
+    String fNumber="";
+    
     @FXML
-    void goBack (ActionEvent event) throws IOException
-    {
-        AnchorPane pane = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-        flightMakerPane.getChildren().setAll(pane);
-    }
-
-    @FXML
-    void flightMaker(ActionEvent event) throws FileNotFoundException, IOException {
-
+    void flightMaker(ActionEvent event) throws FileNotFoundException, IOException {        
+        
         //Below are the variables/entries entered into/retrieved from application
         String fNumber = flightNum.getText();
         String fDate = flightDate.getText();
@@ -80,10 +67,10 @@ public class MakeFlightController implements Initializable {
         String aCity = DestCity.getText();
         String numOfSeats = AvailableSeats.getText();
         System.out.println("made it this far"); //for debugging
-
+        
         //arraylist that contains the information
         ArrayList<String> flightReservation = new ArrayList<String>();
-
+        
         //adding flight info into arraylist
         flightReservation.add(fNumber);
         flightReservation.add(fDate);
@@ -92,12 +79,12 @@ public class MakeFlightController implements Initializable {
         flightReservation.add(dCity);
         flightReservation.add(aCity);
         flightReservation.add(numOfSeats);
-
-        System.out.println("file created"); //for debugging
-
+        
+        System.out.println("file created"); //for debugging 
+        
         //create filewriter to write to flights.txt file
         FileWriter flights = new FileWriter("flights.txt");
-        flights.write("Flight #\t FDate\t\t DTime\t\t ATime\t\t DepartCity\t\t DestCity\t\t AvailableSeats\n\n");
+        flights.write("Flight #\t FDate\t\t DTime\t\t ATime\t\t DepartCity\t\t DestCity\t\t AvailableSeats\n");
         flights.write(System.getProperty("line.separator"));
         for(String str: flightReservation)
         {
@@ -105,14 +92,66 @@ public class MakeFlightController implements Initializable {
             flights.write("\t\t");
         }
         flights.close();
-
+        
         System.out.println("Contents added"); //for debugging
+        
+        mapMaker(fNumber);
+        
+        System.out.println("Sending to mapMaker function"); //for debugging
+    }
+    
+    @FXML
+    public void mapMaker(String mapName) throws IOException{
+        
+        System.out.println("MapMaker part1"); //for debugging
+        FileWriter map = new FileWriter(fNumber+".txt"); //need to name after fNumber 
+        char[][] fNumber = new char[8][10]; //create a map of 7 columns by 10 rows
+        System.out.println("Map created"); //for debugging
+        
+        
+
+        int count=1;
+        
+            for(int row=0;row<=9;row++){
+                char alphabet = 'A';
+                map.write(count+"\t");
+                
+                for(int col=0;col<2;col++){ //prints 'ab' before tabbing over
+                    fNumber[col][row] = alphabet;
+                    map.write(fNumber[col][row]+' ');
+                    System.out.print(fNumber[col][row]);//for debugging
+                    alphabet++;
+                }
+                map.write(' ');
+                
+                for(int col=3;col<6;col++){ //print 'cde' before tabbing over
+                    fNumber[col][row] = alphabet;
+                    map.write(fNumber[col][row]+' ');
+                    System.out.print(fNumber[col][row]);//for debugging
+                    alphabet++;
+                }
+                map.write(' ');
+                
+                for(int col=6;col<8;col++){ //prints 'fg' before tabbing over
+                    fNumber[col][row] = alphabet;
+                    map.write(fNumber[col][row]+' ');
+                    System.out.print(fNumber[col][row]);//for debugging
+                    alphabet++;
+                }           
+                count++;
+                map.write(System.getProperty("line.separator"));//skips line in file
+                System.out.println("\n");//for debugging
+            }            
+        
+        System.out.println("Map filled"); //for debugging
+        map.close();
+        System.out.println("MapMaker closed"); //for debugging
     }
 
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }
-
+    }    
+    
 }
