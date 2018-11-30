99a51d1067c39119481b67dc082cfa0c1f89a2e9
JamesHolland181 <42613313+JamesHolland181@users.noreply.github.com>
2018-11-30 18:37:57 -0500

    AllFlights 2.0
    AllFlightsController works great, as far as I know. Prints all flights from file into text area. Also see new fxml.

--- AllFlightsController.java
+++ AllFlightsController.java
@@ -5,18 +5,18 @@
  */
 package airplane;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.Button;
-import javafx.scene.control.Slider;
-import javafx.scene.control.TextField;
-
-import javafx.beans.value.*;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.AnchorPane;
 
@@ -28,10 +28,13 @@ import javafx.scene.layout.AnchorPane;
 public class AllFlightsController implements Initializable {
 
     @FXML
-    private AnchorPane flightMapPane;
-    
-    @FXML
     private Button backButton;
+
+    @FXML
+    private Label allFlightsLabel;
+
+    @FXML
+    private AnchorPane flightMapPane;
     
     @FXML
     void goBack(ActionEvent event) throws IOException
@@ -40,8 +43,34 @@ public class AllFlightsController implements Initializable {
         flightMapPane.getChildren().setAll(pane);        
     }
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+    public void initialize(URL url, ResourceBundle rb){
+        try {
+            loadAllFlights();
+        } catch (IOException ex) {
+            Logger.getLogger(AllFlightsController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }   
+    
+    @FXML
+    void loadAllFlights() throws IOException{
+        
+        //create StringBuilder called seatingMap
+        StringBuilder allFlights = new StringBuilder();
+        //set filePath to be used for grabbing the file
+        String filePath = "C:\\Users\\Owner\\Desktop\\Programming2\\GroupProject\\airplane_2\\allFlights.txt";
+        System.out.println("Sending to "+filePath);
+        
+            try (BufferedReader br = new BufferedReader(new FileReader(filePath)))
+            {
+                String sCurrentLine; while ((sCurrentLine = br.readLine()) != null)
+                {
+                    allFlights.append(sCurrentLine).append("\n");
+                }
+            }
+        System.out.println("Made it past bufferedreader");
+        System.out.println(allFlights+"\nSuccessfully Printed!\n");//printing seating map to screen for debugging
+        
+        allFlightsLabel.setText(allFlights.toString()); 
+    }
     
 }
