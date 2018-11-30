0d74fc9c6041b17f310ca86389a0ec95c784732a
JamesHolland181 <42613313+JamesHolland181@users.noreply.github.com>
2018-11-30 18:51:00 -0500

    Update specificFlightsController.java
    Functions correctly but has issues.
    
    **Issues**
    
    Loads flight map rather than flight reservations

--- specificFlightsController.java
+++ specificFlightsController.java
@@ -5,6 +5,9 @@
  */
 package airplane;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -13,10 +16,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.Button;
-import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 
-import javafx.beans.value.*;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.AnchorPane;
 
@@ -30,9 +31,18 @@ public class specificFlightsController implements Initializable
 
     @FXML
     private Button backButton;
+    
+    @FXML
+    private TextField flightNumberField;
 
     @FXML
     private AnchorPane allReservationPane;
+    
+    @FXML
+    private Button enterButton;
+    
+    @FXML
+    private Label reservationLabel;    
 
     @FXML
     void goBack(ActionEvent event) throws IOException
@@ -40,6 +50,31 @@ public class specificFlightsController implements Initializable
         AnchorPane pane = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         allReservationPane.getChildren().setAll(pane);
     }
+    
+    @FXML
+    void enterFlightNumber(ActionEvent event) throws FileNotFoundException, IOException{
+        
+        String fNumber = flightNumberField.getText();
+        //create StringBuilder called seatingMap
+        StringBuilder seatingMap = new StringBuilder();
+        //set filePath to be used for grabbing the file
+        String filePath = "C:\\Users\\Owner\\Desktop\\Programming2\\GroupProject\\airplane_2\\Flight #"+fNumber+".txt";
+        System.out.println("Sending to "+filePath+". Filename: "+fNumber);
+        
+            try (BufferedReader br = new BufferedReader(new FileReader(filePath)))
+            {
+                String sCurrentLine; while ((sCurrentLine = br.readLine()) != null)
+                {
+                    seatingMap.append(sCurrentLine).append("\n");
+                }
+            }
+        System.out.println("Made it past bufferedreader");
+        System.out.println(seatingMap+"\nSuccessfully Printed!\n");//printing seating map to screen for debugging
+        
+        reservationLabel.setText(seatingMap.toString()); //Cannot get Label to display on scene pane, causes crash        
+        
+    }
+    
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
