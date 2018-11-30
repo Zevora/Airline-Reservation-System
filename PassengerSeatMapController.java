6d96f59fa79183ef8836c8850dd29439b6b814b4
JamesHolland181 <42613313+JamesHolland181@users.noreply.github.com>
2018-11-30 18:54:54 -0500

    Update PassengerSeatMapController.java

--- PassengerSeatMapController.java
+++ PassengerSeatMapController.java
@@ -15,12 +15,9 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.Button;
-import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 
-import javafx.beans.value.*;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.TextArea;
 import javafx.scene.layout.AnchorPane;
 
 /**
